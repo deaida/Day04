b34984ce7cce7e702f3fb80dd6bf069919c27409
deaida <aida_b_95@hotmail.com>
2021-03-11 21:48:35 +0100

    division

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,17 @@
+function main (numOne, numTwo){
+    var quotient;
+    if (numTwo==0){
+        quotient= "Devide by 0! cannot divide by zero!";
+    } else{
+        //Only change code below this line
+var quotient=numOne/numTwo;
+        //Only change code above this line 
+    }
+    return quotient;
+}
+console.log(main(15, 3));
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(50, 0));
+module.exports=main;
 
