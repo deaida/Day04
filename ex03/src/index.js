d5460bf28116634170f15be6086d435fa24e873e
deaida <aida_b_95@hotmail.com>
2021-03-11 23:26:55 +0100

    decrement

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,10 @@
+function main(){
+    var myRes =16;
+    //only change code below this line
 
+console.log(myRes--);
+    //Only change code above this line
+    return myRes;
+}
+console.log(main());
+module.exports=main;
