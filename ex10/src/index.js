bf191980b7c8a7bc8faa5d90e6684b5a009baa38
deaida <aida_b_95@hotmail.com>
2021-03-12 06:54:43 +0100

    Declare string

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,15 @@
+function main(){
+    //Only change code below this line
+    var myFirstName = "Aida"
+    var myLastName = "Delic"
+    var myHomeTown = "Konjic"
+    //Only change code above this line 
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports=main;
 
